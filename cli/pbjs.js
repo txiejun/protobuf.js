8e519ca920672ec8e3a74f820f4ab73fefd8fd31
dcodeIO <dcode@dcode.io>
2016-11-14 16:09:12 +0100

    Actually don't need sources anymore, initial usage options

--- cli/pbjs.js
+++ cli/pbjs.js
@@ -1,38 +1,39 @@
 var minimist = require("minimist"),
     path     = require("path"),
+    fs       = require("fs"),
     resolve  = require("resolve-from"),
     chalk    = require("chalk");
 
 var protobuf = require(".."),
     util     = require("./util"),
-    sources  = util.requireAll("./sources"),
     targets  = util.requireAll("./targets"),
     pkg      = require("../package.json");
 
 exports.main = function(args) {
     var argv = minimist(args.slice(2), {
         alias: {
-            source: "s",
             target: "t",
+            out   : "o",
             path  : "p"
         },
-        string: [ "source", "target", "path" ],
+        string: [ "target", "out", "path" ],
         default: {
-            source: "proto",
             target: "json"
         }
     });
-    var source = sources[argv.source],
-        target = targets[argv.target],
+    var target = targets[argv.target],
         files  = argv._;
 
-    if (!source || !target || !files.length) {
+    if (!target || !files.length) {
         console.log([
             "protobuf.js v" + pkg.version + " cli",
             "",
-            "Converts between proto and json file formats.",
+            "Converts between file formats.",
             "",
-            "usage: " + chalk.bold.green(path.basename(process.argv[1])) + " [--source " + chalk.bold.white("proto") + "|json] [--target " + chalk.bold.white("json") + "|proto3] file1.proto file2.proto ..."
+            "  -t, --target    Specifies the target format. [" + Object.keys(targets).join(', ') + "]",
+            "  -o, --out       Saves to a file instead of writing to stdout.",
+            "",
+            "usage: " + chalk.bold.green(path.basename(process.argv[1])) + " [options] file1.proto file2.proto ..."
         ].join("\n"));
         return 1;
     }
@@ -45,16 +46,21 @@ exports.main = function(args) {
         return protobuf.util.resolvePath(origin, target);
     };
 
-    var sourceOptions = {},
-        targetOptions = {};
+    var options = {};
 
-    source(root, files, sourceOptions, function(err, root) {
+    root.load(files, function(err) {
         if (err)
             throw err;
-        target(root, targetOptions, function(err, output) {
+        target(root, options, function(err, output) {
             if (err)
                 throw err;
-            process.stdout.write(output, "utf8");
+            if (output !== "") {
+                if (argv.out)
+                    fs.writeFileSync(argv.out, output, { encoding: "utf8" });
+                else
+                    process.stdout.write(output, "utf8");
+            }
+            process.exit(0);
         });
     });
 };
